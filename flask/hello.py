e7aaf41b31705170142f043506eb3378a2edbbba
Jack <lijie9228@hotmail.com>
2017-05-25 11:08:29 +0800

    Deployment Options

--- flask/hello.py
+++ flask/hello.py
@@ -1,6 +1,44 @@
 from flask import Flask
+from flask import request
+
+
 app = Flask(__name__)
 
+# with app.request_context(environ):
+#     assert request.method == 'POST'
+
+# @app.route('/')
+# def index():
+#     username = request.cookies.get('username')
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'Index Page'
+
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    # return render_template('login.html', error=error)
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['the_file']
+        f.save('/var/www/uploads/uploaded_file.txt')
+
+
+        
