9c8f60de975615d4d0663544103c0144ae1b725c
Jack <lijie9228@hotmail.com>
2017-06-01 07:50:16 +0800

    同步 Macbook Pro

--- flask/hello.py
+++ flask/hello.py
@@ -1,18 +1,59 @@
-from flask import Flask
+from flask import Flask, jsonify, abort, make_response
 from flask import request
 
 
 app = Flask(__name__)
 
+
+if __name__ == '__main__':
+    app.run(debug = True)
 # with app.request_context(environ):
 #     assert request.method == 'POST'
 
 # @app.route('/')
 # def index():
 #     username = request.cookies.get('username')
+
+
+tasks = [
+  {
+      'id': 1,
+      'title': u'Buy groceries',
+      'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+      'done': False
+  },
+  {
+      'id': 2,
+      'title': u'Learn Python',
+      'description': u'Need to find a good Python tutorial on the web',
+      'done': False
+  },
+]
+
+@app.route('/todo/tasks', methods = ['POST', 'GET'])
+def get_tasks():
+    if request.method == 'POST':
+        return jsonify({'tasks': tasks})
+
+# if __name__ == '__main__':
+#   app.run(debug = True)
+
+@app.route('/todo/tasks/<int:task_id>', methods = ['POST', 'GET'])
+def get_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Not found' } ), 404)
+
+
 @app.route('/')
 def index():
-    return 'Index Page'
+    return 'Index Page ha'
 
 
 @app.route('/hello')
@@ -39,6 +80,9 @@ def upload_file():
     if request.method == 'POST':
         f = request.files['the_file']
         f.save('/var/www/uploads/uploaded_file.txt')
+
+
+
 
 
         
