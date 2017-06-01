1372b86fb994211feaa04d2a63a81441573c4889
Jack <lijie9228@hotmail.com>
2017-06-01 21:57:16 +0800

    删掉flask文件，修正一些问题

--- flask/hello.py
+++ flask/hello.py
@@ -1,12 +1,77 @@
-from flask import Flask, jsonify, abort, make_response
-from flask import request
+from flask import Flask, jsonify, abort, make_response, url_for, request
+# from flask import request 
+from flask.ext.restful import Api, Resource
+from flask.ext.restful import reqparse
+from passlib.apps import custom_app_context as pwd_context
+
+from flask.ext.sqlalchemy import SQLAlchemy
+# from flask.db import db
 
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
 if __name__ == '__main__':
     app.run(debug = True)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(32), index = True)
+    password_hash = db.Column(db.String(128))
+        # ...
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+
+@app.route('/api/users', methods = ['POST'])
+def new_user():
+    # print request
+    # if len(task) == 0:
+    #     abort(404)
+    if not request.json:
+        abort(400)
+    if 'username' in request.json and type(request.json['username']) != unicode:
+        abort(400)
+    username = request.json.get('username')
+    password = request.json.get('password')
+    # if username is None or password is None:
+    #     abort(400) # missing arguments
+    # if User.query.filter_by(username = username).first() is not None:
+    #     abort(400) # existing user
+    user = User(username = username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({ 'username': user.username }), 201, {'Location': url_for('get_user', id = user.id, _external = True)}
+
+
+
+class TaskListAPI(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type = str, required = True,
+            help = 'No task title provided', location = 'json')
+        self.reqparse.add_argument('description', type = str, default = "", location = 'json')
+        super(TaskListAPI, self).__init__()
+
+    # ...
+
+class TaskAPI(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type = str, location = 'json')
+        self.reqparse.add_argument('description', type = str, location = 'json')
+        self.reqparse.add_argument('done', type = bool, location = 'json')
+        super(TaskAPI, self).__init__()
+
+
 # with app.request_context(environ):
 #     assert request.method == 'POST'
 
