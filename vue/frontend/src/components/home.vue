149ad03680b63f7139efd595f8918d4123479c5e
Jack <lijie9228@hotmail.com>
2018-03-14 14:54:41 +0800

    vue test2

---
+++ vue/frontend/src/components/home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="http://localhost:5000/api/upload"
+          target="_blank"
+        >
+          上传图片
+        </a>
+      </li>
+
+      <li>
+        <a
+          href="/classify"
+        >
+          分类
+        </a>
+      </li>
+
+      <li>
+        <a
+          href="/mine"
+          target="_blank"
+        >
+          我的
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+      msg: '上传'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b9de;
+}
+</style>
---
+++ vue/frontend/src/components/mine.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2></h2>
+    <ul>
+      <li>
+        <a
+          href="/mine/"
+          target="_blank"
+        >
+          我的
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Mine',
+  data () {
+    return {
+      msg: '我的'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b9de;
+}
+</style>
